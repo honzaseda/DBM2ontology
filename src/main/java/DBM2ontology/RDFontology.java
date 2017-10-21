575123f53187468a7024e87ef387f931766e6f8c
honzaseda <honza.seda@email.cz>
2017-10-21 16:21:25 +0200

    Cleaning

--- src/main/java/DBM2ontology/RDFontology.java
+++ src/main/java/DBM2ontology/RDFontology.java
@@ -1,18 +1,11 @@
 package DBM2ontology;
 
-import org.apache.jena.ontology.Individual;
-import org.apache.jena.ontology.OntClass;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.*;
 
-import org.apache.jena.util.FileManager;
-import org.apache.jena.util.iterator.ExtendedIterator;
 import org.apache.jena.vocabulary.RDF;
 
 import java.io.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 /**
  * Created by seda on 19/10/17.
@@ -38,26 +31,6 @@ public class RDFontology {
 
             FileOutputStream f = new FileOutputStream(new File("data/ontology.xml"));
             ontModel.write(f);
-
-//            StmtIterator resIterator = model.listStatements();
-//            while(resIterator.hasNext()){
-//                Statement statement = iter.nextStatement();
-//                Resource  subject   = statement.getSubject();     // get the subject
-//                Property  predicate = statement.getPredicate();   // get the predicate
-//                RDFNode   object    = statement.getObject();      // get the object
-//
-//                System.out.print(subject.toString());
-//                System.out.print(" " + predicate.toString() + " ");
-//                if (object instanceof Resource) {
-//                    System.out.print(object.toString());
-//                } else {
-//                    System.out.print(" \"" + object.toString() + "\"");
-//                }
-//                System.out.println(" .");
-//            }
-            System.out.println("Blboune");
-
-
         } catch (IOException e) {
             System.err.println(e);
         }
