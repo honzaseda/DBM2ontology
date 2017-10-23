8dfc7572e7771ccc7fe5944446d76743ec6eb77f
honzaseda <honza.seda@email.cz>
2017-10-23 16:25:42 +0200

    Restrukturovanej kód. Zjištění property range.

--- src/main/java/DBM2ontology/RDFontology.java
+++ src/main/java/DBM2ontology/RDFontology.java
@@ -1,16 +1,15 @@
 package DBM2ontology;
 
-import org.apache.jena.graph.Node;
 import org.apache.jena.ontology.DatatypeProperty;
 import org.apache.jena.ontology.ObjectProperty;
 import org.apache.jena.ontology.OntModel;
 import org.apache.jena.rdf.model.*;
 
 import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.XSD;
+import org.apache.xerces.util.URI;
 
 import java.io.*;
-import java.util.Properties;
-import java.util.Set;
 
 /**
  * Created by seda on 19/10/17.
@@ -25,45 +24,64 @@ public class RDFontology {
 
             Model model = ModelFactory.createDefaultModel();
             OntModel ontModel = ModelFactory.createOntologyModel();
-
-            model.read(new FileInputStream(inputFileName), null, "TTL");
-            ResIterator resIterator = model.listResourcesWithProperty(nodeType);
-
             ontModel.setNsPrefixes(model.getNsPrefixMap());
+            model.read(new FileInputStream(inputFileName), null, "TTL");
 
+            System.out.println("Generating classes");
+            ResIterator resIterator = model.listResourcesWithProperty(nodeType);
             while (resIterator.hasNext()) {
                 ontModel.createClass(resIterator.next().getProperty(nodeType).getObject().toString());
             }
-
-//            ontModel.createObjectProperty()
-
+            System.out.println("Classes generated");
             StmtIterator stmtIterator = model.listStatements();
+            System.out.println("Generating objects and their properties");
             while (stmtIterator.hasNext()) {
                 Statement stmt = stmtIterator.nextStatement();
                 RDFNode object = stmt.getObject();
-                ResIterator propertySubject = model.listSubjectsWithProperty(stmt.getPredicate());
                 if (object instanceof Resource) {
                     ObjectProperty objectProperty = ontModel.createObjectProperty(stmt.getPredicate().toString());
-                    while(propertySubject.hasNext()){
-                        objectProperty.addDomain(propertySubject.next().getProperty(RDF.type).getObject().asResource());
-                    }
-//                    System.out.println(object.toString() + " is an object type");
+                    objectProperty.addDomain(stmt.getSubject().getProperty(RDF.type).getObject().asResource());
+                    objectProperty.addRange(getRange(object.toString()));
                 } else {
                     DatatypeProperty datatypeProperty = ontModel.createDatatypeProperty(stmt.getPredicate().toString());
-                    while(propertySubject.hasNext()){
-                        datatypeProperty.addDomain(propertySubject.next().getProperty(RDF.type).getObject().asResource());
-                    }
-
-//                    System.out.println(object.toString() + " is an data type");
+                    datatypeProperty.addDomain(stmt.getSubject().getProperty(RDF.type).getObject().asResource());
+                    datatypeProperty.addRange(getRange(object.toString()));
                 }
             }
-
-            Properties properties = new Properties();
-
+            System.out.print("Done. ");
             FileOutputStream f = new FileOutputStream(new File(outputFileName));
             ontModel.write(f);
+            System.out.println("Saved output to file.");
         } catch (IOException e) {
             System.err.println(e);
         }
     }
+
+    private static Resource getRange(String objectValue){
+        if (objectValue.contains(".")) {
+            try {
+                double dbl = Double.parseDouble(objectValue);
+                return XSD.xdouble;
+            } catch (Exception ex) {/* .. */}
+        }
+
+        try {
+            int integer = Integer.parseInt(objectValue);
+            return XSD.integer;
+        } catch (Exception ex) {/* .. */}
+
+        try {
+            long lng = Long.parseLong(objectValue);
+            return XSD.xlong;
+        } catch (Exception ex) {/* .. */}
+
+        try{
+            if(objectValue.contains(":") ||  objectValue.contains(".") || objectValue.contains("/")) {
+                URI uri = new URI(objectValue);
+                return XSD.anyURI;
+            }
+        } catch (Exception ex){/* .. */}
+        return XSD.xstring;
+    }
+
 }
