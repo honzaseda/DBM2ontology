3028abd8cfcf000b5771d08e03942b93e0bcc08e
honzaseda <honza.seda@email.cz>
2017-11-03 18:58:31 +0100

    reworked dataTypeProperty range prediction

--- src/main/java/DBM2ontology/RDFontology.java
+++ src/main/java/DBM2ontology/RDFontology.java
@@ -1,10 +1,12 @@
 package DBM2ontology;
 
+import org.apache.jena.ext.com.google.common.primitives.Doubles;
+import org.apache.jena.ext.com.google.common.primitives.Ints;
+import org.apache.jena.ext.com.google.common.primitives.Longs;
 import org.apache.jena.ontology.*;
 import org.apache.jena.rdf.model.*;
 
 import org.apache.jena.util.iterator.ExtendedIterator;
-import org.apache.jena.vocabulary.OWL;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.XSD;
 import org.apache.xerces.util.URI;
@@ -13,39 +15,68 @@ import java.io.*;
 import java.util.*;
 
 /**
- * Created by seda on 19/10/17.
+ * Created by Seda on 19/10/17.
  */
 public class RDFontology {
     static private Property nodeType = RDF.type;
-    static private String inputFileName = "data/20171009-eu-6.ttl";
-    static private String outputFileName = "data/ontology.xml";
+    static private String inputFileName;
+    static private String outputFileName;
 
     static private Model model;
     static private OntModel ontModel;
 
     public static void main(String[] args) {
+        if(!validArgs(args)){
+            return;
+        }
+
+        model = ModelFactory.createDefaultModel();
+        ontModel = ModelFactory.createOntologyModel();
+        ontModel.setNsPrefixes(model.getNsPrefixMap());
+
+        model.read(inputFileName);
+
+        System.out.println("Creating classes...");
+        createClassesFromResources(model.listResourcesWithProperty(nodeType));
+
+        System.out.println("Creating Properties...");
+        createPropertiesFromStatements(model.listStatements());
+
+        System.out.print("Done. ");
+        writeOntology(outputFileName);
+    }
+
+    private static boolean validArgs(String[] args){
+        if(args.length < 1){
+            System.out.println("You must specify an input file name as argument.");
+            return false;
+        }
+        else{
+            inputFileName = args[0];
+        }
+
+        if(args.length > 1){
+            outputFileName = args[1];
+        }
+        else{
+            outputFileName = "data/ontology.owl";
+        }
+        return true;
+    }
+
+    private static void writeOntology(String fileName) {
         try {
-
-            model = ModelFactory.createDefaultModel();
-            ontModel = ModelFactory.createOntologyModel();
-            ontModel.setNsPrefixes(model.getNsPrefixMap());
-
-            model.read(inputFileName);
-
-            System.out.println("Creating classes...");
-            ResIterator resIterator = model.listResourcesWithProperty(nodeType);
-            while (resIterator.hasNext()) {
-                ontModel.createClass(resIterator.next().getProperty(nodeType).getObject().toString());
-            }
-
-            System.out.println("Creating Properties...");
-            createPropertiesFromStatements(model.listStatements());
-            System.out.print("Done. ");
-            FileOutputStream f = new FileOutputStream(new File(outputFileName));
+            FileOutputStream f = new FileOutputStream(new File(fileName));
             ontModel.write(f);
             System.out.println("Ontology saved to file.");
         } catch (IOException e) {
             System.err.println(e);
+        }
+    }
+
+    private static void createClassesFromResources(ResIterator resIterator) {
+        while (resIterator.hasNext()) {
+            ontModel.createClass(resIterator.next().getProperty(nodeType).getObject().toString());
         }
     }
 
@@ -56,11 +87,11 @@ public class RDFontology {
             if (object instanceof Resource) {
                 ObjectProperty objectProperty = ontModel.createObjectProperty(stmt.getPredicate().toString(), checkPropertyFunctionality(stmt.getPredicate()));
                 objectProperty.addDomain(stmt.getSubject().getProperty(nodeType).getObject().asResource());
-                objectProperty.addRange(getObjectRange(stmt.getPredicate().getLocalName()));
+                objectProperty.setRange(getObjectRange(stmt.getPredicate().getLocalName()));
             } else {
                 DatatypeProperty datatypeProperty = ontModel.createDatatypeProperty(stmt.getPredicate().toString(), checkPropertyFunctionality(stmt.getPredicate()));
                 datatypeProperty.addDomain(stmt.getSubject().getProperty(nodeType).getObject().asResource());
-                datatypeProperty.addRange(getDataRange(object.toString()));
+                datatypeProperty.setRange(getDataRange(object.toString()));
             }
         }
     }
@@ -100,6 +131,24 @@ public class RDFontology {
     }
 
     private static Resource getDataRange(String objectValue) {
+        if (objectValue.contains(".")) {
+            if (Doubles.tryParse(objectValue) != null) {
+                return XSD.xdouble;
+            }
+        }
+
+        if (Ints.tryParse(objectValue) != null) {
+            return XSD.integer;
+        }
+
+        if (Longs.tryParse(objectValue) != null) {
+            return XSD.xlong;
+        }
+
+        return XSD.xstring;
+    }
+
+    private static Resource oldGetDataRange(String objectValue) {
         if (objectValue.contains(".")) {
             try {
                 double dbl = Double.parseDouble(objectValue);
